725c8cbb3c24478b5e1201c9f4af1ae03e852107
jdspell <spelljd15@gmail.com>
2019-03-05 12:20:12 -0500

    added inputs and started to implement the redux data flow

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import TodoForm from './components/TodoForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>ToDo List</h1>
+        <TodoForm />
       </div>
     );
   }
